284c37702c47c43e815730628c349c367275ff7b
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-30 15:12:26 +0200

    added all img elements to carousel container

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,7 @@
 const images = [
     {
         image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
+        title: 'Marvel\'s Spiderman Miles Morales',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
         image: 'img/02.webp',
@@ -21,3 +21,23 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+const carousel = document.getElementById('carousel');
+
+images.forEach(game => addElement('img', '', carousel).src = game.image);
+
+
+/* FUNCTIONS */
+
+/**
+ * This function creates an html element of the chosen type and class, with the chosen text, and adds it at the end of the selected container.
+ * @param {*} type The type of html element that will be created. 
+ * @param {*} innerText The text that the created element will have inside.
+ * @param {*} container The container at the end of which the element will be added.
+ */
+function addElement(type, innerText, container) {
+    type = document.createElement(type);
+    type.innerHTML = innerText;
+    container.append(type);
+    return type;
+}
